07f07a63acef6031312f44725ba80cee25da02ed
analaura100 <19470201@itcampeche.edu.mx>
2020-11-05 00:42:32 -0600

    Version inicial del proyecto. Utiliza manejo de eventos para funcionar.

---
+++ Databindingexample/EventModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databindingexample
+{
+    class EventModel
+    {
+        private string title;
+        private DateTime date;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (value != title)
+                {
+                    title = value;
+                }
+            }
+        }
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                if (value !=date)
+                {
+                    date = value;
+                }
+            }
+        }
+        
+
+    }
+}
---
+++ Databindingexample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Databindingexample
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EventModel evento;
+        public MainWindow()
+        {
+            InitializeComponent();
+            evento = new EventModel()
+            {
+                Title = "Reunión de trabajo",
+                Date = new DateTime(2020, 11, 05)
+            };
+            nombretextbox.Text = evento.Title;
+            fechadatepicker.SelectedDate = evento.Date;
+        }
+
+        private void modificareventobutton_Click(object sender, RoutedEventArgs e)
+        {
+            evento.Title = evento.Title.ToLower();
+            evento.Date = evento.Date.AddDays(1); 
+            nombretextbox.Text = evento.Title; 
+            fechadatepicker.SelectedDate = evento.Date; 
+            MessageBox.Show(evento.Title + "\n" + evento.Date);
+        }
+    }
+}
